0f81166301dda2b12bee68a27e773bd83c7783ad
longtogoonce <goto.age@gmail.com>
2023-07-28 21:51:10 +0800

    Group

--- Client/View/Group_UI.hpp
+++ Client/View/Group_UI.hpp
@@ -1,6 +1,7 @@
 #ifndef GROUP_UI_H_
 #define GROUP_UI_H_
 #include <string>
+#include "../../Common/Account.hpp"
 using namespace std;
 
 /*
@@ -9,26 +10,11 @@ using namespace std;
  *                      管理人员对普通用户的管理
  */
 
-
 //初始群界面
-void Group_UI_initEntry();
+void Group_UI_initEntry();      
 //群管理界面
 void Group_UI_MgtEntry();
 //群聊界面
 void Group_UI_ChatEntry();
-
-
-// 创建群组
-int Group_Srv_CreatGroup();
-//解散群组
-int Group_Srv_DelGroup(string name);
-//加入群组
-int Group_Srv_AddGroup(string name);
-//退出群组
-int Group_Srv_ExitGroup(string name);
-
-//增加/删除管理员
-//批准加入/移除
-//查看成员列表
 
 #endif
