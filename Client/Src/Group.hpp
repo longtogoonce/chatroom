3a6788c6ce2f8c76d2e3ac4bd6ae2c117d692688
longtogoonce <goto.age@gmail.com>
2023-08-01 22:01:49 +0800

    Group

---
+++ Client/Src/Group.hpp
@@ -0,0 +1,27 @@
+#ifndef GROUP_H_
+#define GROUP_H_
+#include <string>
+#include <vector>
+#include "../../Common/Message.hpp"
+using namespace std;
+
+// 创建群组
+void Group_Srv_CreatGroup();
+//解散群组
+void Group_Srv_DelGroup(string name);
+//加入群组
+void Group_Srv_AddGroup();    
+//退出群组
+void Group_Srv_ExitGroup(string name);
+//获取群列表
+vector<string> Group_Srv_GetList(string name);
+//查看群聊申请
+vector<string> Group_Srv_GetApply(string name);
+//删除用户
+void Group_Srv_Deluser(string name);
+//添加管理者
+void Group_Srv_AddManager(string name);
+//移除管理者
+void Group_Srv_DelManager(string name);
+
+#endif
