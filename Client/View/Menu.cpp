7f581614bb75eead4439c7859160dd37ae7d7b34
longtogoonce <goto.age@gmail.com>
2023-08-02 22:06:26 +0800

    Nenu

--- Client/View/Menu.cpp
+++ Client/View/Menu.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include "Account.hpp"
-#include "TcpSocket.hpp"
 #include "../Src/Login.hpp"
 #include "Login_UI.hpp"
 #include "Friend_UI.hpp"
 #include "Group_UI.hpp"
+#include "../../Common/TcpSocket.hpp"
+#include "../../Common/Account.hpp"
 #include "../../Common/PutFormat.hpp"
-
-using namespace std;
-
-TcpSocket Socketfd;   //全局套接字
-Account Curuser;    //当前登陆用户
+    
+TcpSocket Socketfd;   //全局套接字  
 PutFormat put;  //规范输出
+Account Curuser;    //当前登陆用户
+using namespace std;
 
 void Main_Menu(void)
 {
@@ -22,36 +21,37 @@ void Main_Menu(void)
 
     //连接服务器，开始登陆
     Socketfd.ConnectToHost(IP, 9000);
-    
-begin:  if(SysLogin()==0)
-		return ;
+begin:
+    if (SysLogin() == 0)
+        return ;
 	
     //登陆成功，进入初始菜单
 	char choice;
     do
     {
         system("clear");
-        cout << '\n' << "\t\t===================================================================" << endl;
-        cout << "\t\t*********************** xiaotian's chatroom ***********************" << endl;
+        put.printFrommid2("===================================================================");
+        put.printFrommid2("*********************** xiaotian's chatroom ***********************");
+        put.printFrommid2("===================================================================");
         cout << "" << endl;
         cout << "" << endl;
         cout << "" << endl;
         cout << "" << endl;
-        cout << "\t\t                   [F]riends               [G]roup                 " << endl;
+        put.printFrommid2("                [F]riends               [G]roup                   ");
         cout << "" << endl;
-        cout << "\t\t                   [D]elete Account        [E]xit                  " << endl;
+        put.printFrommid2("                [D]elete Account        [E]xit                   ");
         cout << "" << endl;
         cout << "" << endl;
         cout << "" << endl;
-        cout << "\t\t===================================================================" << endl;
-        cout <<"\t\tPlease input your choice:" <<endl;
-        cin >> choice;
+        put.printFrommid2("===================================================================");
+        cout <<"\t\tPlease input your choice:";
+        put.stdput(choice);
         switch (choice)
         {
 		case 'F':
 		case 'f':
-			Friend_UI_MgtEntry();
-			break;
+            Friend_UI_MgtEntry();
+            break;
 		case 'G':
 		case 'g':
 			Group_UI_MgtEntry();
