2ae2a0158d3ed022031ba8d7c12788a5f7bf13f3
longtogoonce <goto.age@gmail.com>
2023-08-06 08:41:23 +0800

    Friend

---
+++ Client/Src/Friend.hpp
@@ -0,0 +1,22 @@
+#ifndef FRIEND_H_
+#define FRIEND_H_
+#include "../../Common/Account.hpp"
+#include <string>
+using namespace std;
+
+//好友添加
+void Friend_Srv_Apply();
+//好友删除
+void Friend_Srv_Delete();
+//拉黑用户
+void Friend_Srv_Black();
+//解除拉黑
+void Friend_Srv_NoBlack();
+//获取用户的聊天状态+聊天对象
+vector<string> Friend_Srv_Chat(string name);
+// 获取所有的好友状态
+vector<Friend> Friend_Srv_GetAllstate();
+//获取好友申请列表
+vector<string> Friend_Srv_GetApply();
+
+#endif
