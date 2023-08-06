c676f281bd81e36bef30faa9c4cc1ec80812f8cf
longtogoonce <goto.age@gmail.com>
2023-08-06 22:12:58 +0800

    Menu

--- Client/View/Menu.cpp
+++ Client/View/Menu.cpp
@@ -6,11 +6,16 @@
 #include "../../Common/TcpSocket.hpp"
 #include "../../Common/Account.hpp"
 #include "../../Common/PutFormat.hpp"
-    
+#include "../../Common/Message.hpp"
+#include <thread>
+using namespace std;
+
 TcpSocket Socketfd;   //全局套接字  
 PutFormat put;  //规范输出
 Account Curuser;    //当前登陆用户
-using namespace std;
+MessageQueue msgQueue; // 消息队列
+
+void ReadThreadFromeTcp();  //读线程
 
 void Main_Menu(void)
 {
@@ -21,10 +26,12 @@ void Main_Menu(void)
 
     //连接服务器，开始登陆
     Socketfd.ConnectToHost(IP, 9000);
+    thread ReadThread(ReadThreadFromeTcp);
+    ReadThread.detach();
+
 begin:
     if (SysLogin() == 0)
         return ;
-	
     //登陆成功，进入初始菜单
 	char choice;
     do
@@ -44,7 +51,7 @@ begin:
         cout << "" << endl;
         cout << "" << endl;
         put.printFrommid2("===================================================================");
-        cout <<"\t\tPlease input your choice:";
+        cout <<"\t\t\t\tPlease input your choice:";
         put.stdput(choice);
         switch (choice)
         {
@@ -70,4 +77,25 @@ int main()
 {
     Main_Menu();
     return 0;
+}
+
+void ReadThreadFromeTcp()
+{
+    while(1){
+        string buf = Socketfd.recvMsg();
+        cout << "\n";
+        auto pos = buf.find(":");
+        string state = buf.substr(0, pos);
+        string data = buf.substr(pos + 1);
+        if(!state.compare("ON")){
+            auto pos = data.find("+");
+            string name = data.substr(0, pos);
+            string msg = data.substr(pos + 1);
+            put.printFromLeft(name, color_empty, B_empty, type_empty);
+            put.printFromLeft(msg, black, B_purple, highlight);
+        }else if(!state.compare("OFF"))
+            put.printFrommid("[ "+data+" ]", yellow, B_empty, highlight);
+        else
+            msgQueue.push(data);
+    }
 }
